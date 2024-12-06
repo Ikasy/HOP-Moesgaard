5243ca0b09c147eb4ee957b9eb9a0565097edc83
Julie Nielsen <82449701+Ikasy@users.noreply.github.com>
2024-12-06 12:58:46 +0100

    removed unused project

--- eksperiment_02/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.scrollable').addEventListener('wheel', function (event) {
-    if (event.deltaY !== 0) {  // Kontrollerer, om der er en vertikal scroll
-        this.scrollLeft += event.deltaY;  // Scroll horisontalt baseret på vertikal scroll
-        event.preventDefault();  // Forhindrer standard vertikal scroll
-    }
-});
